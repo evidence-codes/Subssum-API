c81d5f412b16287a54568027b40fe5b695362702
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-08-29 16:11:55 +0100

    feat: create middleware for authenticated requests

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface AuthenticatedRequest extends Request {
+  user?:
+    | {
+        id: string;
+      }
+    | JwtPayload;
+}
+
+const secret = process.env.JWT_SEC || " ";
+
+const auth = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers["authorization"] || "";
+
+  const token = authHeader.split(" ")[1];
+  if (authHeader) {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        throw new Error("Token is not valid!");
+      } else {
+        if (typeof decoded === "string") {
+          req.user = { id: decoded }; // Assigning user ID directly to req.user
+        } else {
+          req.user = decoded as JwtPayload; // Assigning JwtPayload to req.user
+        }
+        next();
+      }
+    });
+  } else {
+    throw new Error("You are not authenticated!");
+  }
+};
+
+export default auth;
